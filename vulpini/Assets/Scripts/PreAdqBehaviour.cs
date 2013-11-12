9d65d00651eee18c612b844372a603b6efc7806e
nicoblakie <nico_blakie@hotmail.com>
2013-11-12 19:20:20 -0300

    Arreglos

--- vulpini/Assets/Scripts/PreAdqBehaviour.cs
+++ vulpini/Assets/Scripts/PreAdqBehaviour.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class PreAdqBehaviour : MonoBehaviour {
 	public Camera cam;	
-	
+	public GameObject pre1;
+	public GameObject pre2;
+	public GameObject pre3;
+	public GameObject pre4;
 	Ray cursorRay;
 	RaycastHit hit;
+	
 	// Use this for initialization
 	void Start () {
 		Statics.Pre1.SetActive(false);
@@ -20,7 +24,6 @@ public class PreAdqBehaviour : MonoBehaviour {
 		{
 			if (Input.GetTouch (0).phase == TouchPhase.Began)
 			{
-				Handheld.Vibrate ();
 				cursorRay = cam.ScreenPointToRay(Input.GetTouch(0).position);			
 	            if( collider.Raycast( cursorRay, out hit, 1000.0f))
 	            {
@@ -29,6 +32,31 @@ public class PreAdqBehaviour : MonoBehaviour {
 					Statics.Pre3.SetActive(true);
 					Statics.Pre4.SetActive(true);
 	            }
+			}
+			
+		    else if(Input.GetTouch(0).phase == TouchPhase.Ended)
+			{
+				cursorRay = cam.ScreenPointToRay(Input.GetTouch(0).position);
+				if(pre1.collider.Raycast(cursorRay,out hit, 1000.0f))
+				{
+					Statics.Player.GetComponent<Avanzar>().Speed=true;
+				}
+				else if(pre2.collider.Raycast(cursorRay, out hit, 1000.0f))
+				{
+					Debug.Log("hoasd");
+				}
+				else if(pre3.collider.Raycast(cursorRay, out hit, 1000.0f))
+				{
+					Debug.Log("hoasd");
+				}
+				else if(pre4.collider.Raycast(cursorRay, out hit, 1000.0f))
+				{
+					Debug.Log("hoasd");
+				}
+				Statics.Pre1.SetActive(false);
+				Statics.Pre2.SetActive(false);
+				Statics.Pre3.SetActive(false);
+				Statics.Pre4.SetActive(false);
 			}
 		}
 	}
