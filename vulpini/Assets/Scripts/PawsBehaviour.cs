c5d2eb391ab82a5c2da909ab0af95c24e200da34
nico_blakie <nico_blakie@hotmail.com>
2013-11-15 13:15:52 -0300

    Arreglo Paws

---
+++ vulpini/Assets/Scripts/PawsBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PawsBehaviour : MonoBehaviour {
+	float time =0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		time += Time.deltaTime; 
+		if(time>= 1)
+		{
+			gameObject.GetComponent<TextMesh>().text = Statics.Paws.ToString();
+			time = 0;
+		}
+	}
+}
